ab397ea52a27a396b518d1e3bf1b96e5f317bd31
kaubu <29447633+kaubu@users.noreply.github.com>
2023-12-17 10:21:57 +0000

    Fixed headers not working via explicit type annotation

--- src/views/Wordlist.vue
+++ src/views/Wordlist.vue
@@ -45,6 +45,7 @@
             :search="search"
             :items-per-page="10"
             multi-sort
+            density="compact"
           >
           </v-data-table>
         </v-card>
@@ -63,7 +64,23 @@ const {
   anglishWordlist
 } = storeToRefs(store);
 
-const headers = [
+const headers: {
+  readonly key?:
+    | 'data-table-group'
+    | 'data-table-select'
+    | 'data-table-expand'
+    | (string & {})
+    | undefined
+  readonly title?: string | undefined
+  readonly fixed?: boolean | undefined
+  readonly align?: 'start' | 'end' | 'center' | undefined
+  readonly width?: string | number | undefined
+  readonly minWidth?: string | undefined
+  readonly maxWidth?: string | undefined
+  readonly headerProps?: { readonly [x: string]: any } | undefined
+  readonly sortable?: boolean | undefined
+  readonly children?: readonly any[] | undefined
+}[] = [
   {
     key: "word",
     title: "Word",
